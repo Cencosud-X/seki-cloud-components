7684a0a3fc5f4f5f4ad05ff942350dfcaac8133b
Maykel Guzmán Carvajal <maykelg2005@gmail.com>
2022-12-22 12:05:53 -0500

    feat(mongodb) add mongodb post install script

---
+++ mongodb/scripts/post.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+
+module.exports = async (runner, args) => {
+  try {
+    console.log('> POST: MongoDB');
+
+    const rc = args.rc;
+    const productPath = path.join(process.cwd());
+    const productConfigPath = path.join(productPath, "config");
+    const settingsJsonPath = path.join(productConfigPath, `${rc.kind}_${rc.identifier}_settings.json`)
+    
+    console.log('> Creating settings file for local environment...');
+    
+    const packageJson = JSON.parse(fs.readFileSync(path.join(productPath, 'package.json'), {encoding:'utf8'}));
+    const data = {
+      connection_strings: `mongodb://localhost:27017/${packageJson.name}_${rc.identifier}`
+    };
+    fs.writeFileSync(settingsJsonPath, JSON.stringify(data, null, 2));
+
+    console.log('> POST: process ✅ DONE');
+
+  } catch (ex) {
+    throw new Error(`Failed run post script (${ex})`);
+  }
+}
