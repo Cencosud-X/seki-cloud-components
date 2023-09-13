aa93672027fe402e692e65e2107ae8e6f2ae37da
Juan Carlos Gómez Correa <jcgomezcorrea@gmail.com>
2023-09-12 23:25:04 -0400

    [Seki CI] deploying dummy

---
+++ dummy/scripts/rollback.js
@@ -0,0 +1,15 @@
+module.exports = async (runner, args) => {
+  try {
+    console.log("> Cleaning...");
+
+    console.log(`args=${JSON.stringify(args)}`)
+
+    await runner.execute([
+      `echo "check this: 'rm -rf ${args.workspacePath}'"`
+    ])
+
+    console.log("> Rollback ✅ DONE");
+  } catch {
+    throw new Error("Failed to rollback");
+  }
+};
